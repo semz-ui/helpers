696e07a2d501e2ae34864f8f2a22387e0dbbd2d2
semz-ui <michaeltimilehin36@gmail.com>
2025-09-15 10:54:21 -0700

    loading and copy added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react"
 import SideBar from "./components/SideBar"
 import EmailRoute from "./routes/EmailRoute"
+import { Toaster } from "./components/ui/sonner"
 
 const App = () => {
   const [route, set_route] =useState<string>("email")
 
  return (
   <div className="flex md:flex-row flex-col min-h-lvh">
+    <Toaster position="top-center" richColors />
     <SideBar route={route} set_route={set_route} />
       <div className="flex justify-center w-dvw">
       <div className="md:w-[480px] w-[380px] min-h-dvh">
--- src/routes/EmailRoute.tsx
+++ src/routes/EmailRoute.tsx
@@ -2,6 +2,7 @@ import EmailEdit from "@/components/email/EmailEdit";
 import EmailInput from "@/components/email/EmailInput";
 import SelectComponent from "@/components/SelectComponent";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
   lengthTypes,
   promptHelper,
@@ -11,6 +12,7 @@ import {
 import { type promptType } from "@/types";
 import { GoogleGenAI } from "@google/genai";
 import { useState } from "react";
+import { toast } from "sonner";
 
 const EmailRoute = () => {
   const [content, set_content] = useState("");
@@ -19,13 +21,16 @@ const EmailRoute = () => {
   const [to_res, set_to_res] = useState("");
   const [tone, set_tone] = useState<promptType>("formal");
   const [length, set_length] = useState("medium");
+  const [loading, set_loading] = useState<boolean>(false);
   const GEMINI_API_KEY = import.meta.env.VITE_API_KEY;
   const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
   // const handleOpenEmailClient = () => {
   //   window.location.href = "mailto:";
   // };
   const handleGenerate = async () => {
-    const response = await ai.models.generateContent({
+    set_loading(true);
+    try {
+      const response = await ai.models.generateContent({
       model: "gemini-2.0-flash-001",
       contents: content,
       config: {
@@ -37,6 +42,11 @@ const EmailRoute = () => {
     set_body_res(seperated.body);
     set_subject_res(seperated.subject);
     set_to_res(seperated.to);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      set_loading(false)
+    }
   };
 
   const handleOpenEmailClient = () => {
@@ -46,30 +56,55 @@ const EmailRoute = () => {
 
     window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
   };
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(body_res);
+     
+       toast("Copy", {
+          description: "Copied successfully to clipboard",
+          action: {
+            label: "done",
+            onClick: () => console.log(""),
+          },
+        })
+    } catch (err) {
+      console.error('Failed to copy:', err);
+    }
+  };
+
   return (
     <div className="w-full min-h-dvh flex justify-center items-center">
       <div className="mx-4 w-full border border-gray-300 rounded-lg p-6">
         <h2 className="text-center font-bold text-lg mb-4">Mailer</h2>
-        <EmailInput
+        {
+          loading ? <Skeleton className="w-full border py-4 px-2 rounded-lg" /> : <EmailInput
           content={content}
           set_content={set_content}
           placeholder="Describe what your email is about"
         />
+        }
         <div className="flex w-full gap-4 mt-4">
-          <SelectComponent
+          {
+            loading ? <Skeleton className="w-full border py-4 rounded-lg" /> : <SelectComponent
             set_value={set_tone}
             label="Tone"
             data={promptTypes}
           />
-          <SelectComponent
+          }
+          {
+            loading ? <Skeleton className="w-full border py-4 rounded-lg" /> :<SelectComponent
             set_value={set_length}
             label="Length"
             data={lengthTypes}
           />
+          }
+          
         </div>
         <Button
-          className="w-full bg-blue-400 py-4 px-2 rounded-lg mt-4 cursor-pointer"
+          className="w-full hover:bg-blue-300 bg-blue-400 py-6 px-2 rounded-lg mt-4 cursor-pointer"
           onClick={handleGenerate}
+          disabled={loading}
         >
           Generate
         </Button>
@@ -83,20 +118,20 @@ const EmailRoute = () => {
         />
         <div className="flex mt-4 justify-end gap-4">
           <Button
-            className="bg-blue-400 py-6 px-4 rounded-lg cursor-pointer"
+            className="bg-blue-400 hover:bg-blue-300 py-6 px-4 rounded-lg cursor-pointer"
             onClick={handleGenerate}
           >
             Regenerate
           </Button>
           <Button
-            onClick={handleOpenEmailClient}
-            className="px-4 py-6 bg-white text-black border border-gray-300"
+            onClick={handleCopy}
+            className="px-4 py-6 hover:bg-gray-200 cursor-pointer bg-white text-black border border-gray-300"
           >
             Copy
           </Button>
           <Button
             onClick={handleOpenEmailClient}
-            className="px-4 py-6 bg-white text-black border border-gray-300"
+            className="px-4 py-6 hover:bg-gray-200 cursor-pointer bg-white text-black border border-gray-300"
           >
             Open Mail
           </Button>
